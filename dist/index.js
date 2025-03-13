1c5a5efd85895b05f413180162901bd312af538d
Eren Kulaksız <erenkulaksz@gmail.com>
2025-03-13 14:14:38 +0300

    feat: sonunda

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,10 @@ import path from "path";
 import chalk from "chalk";
 import { default as player } from "play-sound";
 import { fileURLToPath } from "url";
-// Get the directory where the package is installed
 const packageRoot = path.dirname(fileURLToPath(import.meta.url));
 function main() {
     const soundPath = path.resolve(packageRoot, "..", "sound.m4a");
     player().play(soundPath);
-    console.log(soundPath);
     console.log(chalk.inverse("Abdestlendin!"));
 }
 main();
--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,11 @@ import chalk from "chalk";
 import { default as player } from "play-sound";
 import { fileURLToPath } from "url";
 
-// Get the directory where the package is installed
 const packageRoot = path.dirname(fileURLToPath(import.meta.url));
-
-console.log("packageRoot", packageRoot);
 
 function main() {
   const soundPath = path.resolve(packageRoot, "..", "sound.m4a");
   player().play(soundPath);
-  console.log(soundPath);
   console.log(chalk.inverse("Abdestlendin!"));
 }
 
